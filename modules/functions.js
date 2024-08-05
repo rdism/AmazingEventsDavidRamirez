1d04569720fed142b7d0e33d8679cab1e44a112f
rdism <86022701+rdism@users.noreply.github.com>
2024-08-05 20:32:41 -0300

    Se exportan las funciones obtenerData y categoriasParaChequear

--- modules/functions.js
+++ modules/functions.js
@@ -1,1 +1,17 @@
-//
+export async function obtenerData(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error al obtener los eventos", error);
+    }
+}
+
+export function categoriasParaChequear(eventos) {
+    let categorias = new Set();
+    for (let i = 0; i < eventos.length; i++) {
+        categorias.add(eventos[i].category)
+    }
+    return (categorias);
+}
