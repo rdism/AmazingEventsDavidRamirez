e29a2fab65131bc4197e936b07c3083a4065bb38
rdism <86022701+rdism@users.noreply.github.com>
2024-08-06 02:11:18 -0300

    Se exporta la función mostrarDetallesEvento

--- details.js
+++ details.js
@@ -1,4 +1,4 @@
-import { obtenerData } from "./modules/functions.js";
+import { obtenerData, mostrarDetallesEvento } from "./modules/functions.js";
 
 document.addEventListener("DOMContentLoaded", async () => {
     const data = await obtenerData('https://aulamindhub.github.io/amazing-api/events.json');
@@ -10,27 +10,3 @@ document.addEventListener("DOMContentLoaded", async () => {
         mostrarDetallesEvento(eventDetails);
     }
 });
-
-// Voy a cambiar esta función para que modifique una tarjeta en el html
-function mostrarDetallesEvento(evento) {
-    const contenedor = document.getElementById('detalles');
-    if (evento) {
-        contenedor.innerHTML = `
-            <div class="card mb-4">
-                <img src="${evento.image}" class="card-img-top" alt="${evento.name}">
-                <div class="card-body">
-                    <h5 class="card-title">${evento.name}</h5>
-                    <p class="card-text">${evento.description}</p>
-                    <ul class="list-group list-group-flush">
-                        <li class="list-group-item"><strong>Date:</strong> ${evento.date}</li>
-                        <li class="list-group-item"><strong>Category:</strong> ${evento.category}</li>
-                        <li class="list-group-item"><strong>Place:</strong> ${evento.place}</li>
-                        <li class="list-group-item"><strong>Capacity:</strong> ${evento.capacity}</li>
-                        <li class="list-group-item"><strong>Price:</strong> $${evento.price}</li>
-                    </ul>
-                </div>
-            </div>`;
-    } else {
-        contenedor.innerHTML = `<p>Event not found.</p>`;
-    }
-}
--- modules/functions.js
+++ modules/functions.js
@@ -106,3 +106,26 @@ export function filtrarEventosPorFecha(fechaActual, eventos, filtro) {
 
     pintarTarjetas(eventosFiltrados);
 }
+
+export function mostrarDetallesEvento(evento) {
+    const contenedor = document.getElementById('detalles');
+    if (evento) {
+        contenedor.innerHTML = `
+            <div class="card mb-4">
+                <img src="${evento.image}" class="card-img-top" alt="${evento.name}">
+                <div class="card-body">
+                    <h5 class="card-title">${evento.name}</h5>
+                    <p class="card-text">${evento.description}</p>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><strong>Date:</strong> ${evento.date}</li>
+                        <li class="list-group-item"><strong>Category:</strong> ${evento.category}</li>
+                        <li class="list-group-item"><strong>Place:</strong> ${evento.place}</li>
+                        <li class="list-group-item"><strong>Capacity:</strong> ${evento.capacity}</li>
+                        <li class="list-group-item"><strong>Price:</strong> $${evento.price}</li>
+                    </ul>
+                </div>
+            </div>`;
+    } else {
+        contenedor.innerHTML = `<p>Event not found.</p>`;
+    }
+}
